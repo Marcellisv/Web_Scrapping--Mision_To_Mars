456c654c55946c61c196ab1421a6561ddc198198
Marcellisv <marcellisv@gmail.com>
2021-02-28 18:28:39 -0500

    h

--- Templates/app.py
+++ Templates/app.py
@@ -1,24 +1,25 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+import pymongo
 
-import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 #Create connection 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/Mars_db")
+client = pymongo.MongoClient('mongodb://localhost:27017/')
 
 #Create route to  data form mongodb database
 @app.route("/")
 def home():
-    mars_data = scraped_mars.find()
+    mars_data = scraped_mars.find_one()
     return  render_template('index.html', mars_data=mars_data)
 
 @app.route("/scrape")
 def scrape():
     
-    scrape_mars.scrape()
+    mars_data = client.db.Mars_db
+    mars_sraping = scrape_mars.scrape()
+    mars.update({}, mars_sraping, upsert = True)
 
     return redirect("/")
 
